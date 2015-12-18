04cb49161eb7fa5fedcbf12e0556a9ed0767dd1d
vlzangel <vlzangel@gmail.com>
2015-12-18 12:14:27 -0430

    interface CustomerBrokerPurchaseNegotiationManager
    interface CustomerBrokerPurchaseNegotiationManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseNegotiationManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseNegotiationManager.java
@@ -45,6 +45,14 @@ public interface CustomerBrokerPurchaseNegotiationManager extends FermatManager 
 
     /**
      *
+     * @param negotiationId
+     * @param status
+     * @throws CantUpdateCustomerBrokerPurchaseNegotiationException
+     */
+    void updateNegotiationNearExpirationDatetime(String negotiationId, Boolean status) throws CantUpdateCustomerBrokerPurchaseNegotiationException;
+
+    /**
+     *
      * @param negotiation
      * @throws CantUpdateCustomerBrokerPurchaseNegotiationException
      */
