a9daaad1bf1ee0a65a44d4f0845422bb79ba9350
Manuel <darkpriestrelative@gmail.com>
2016-07-01 06:04:07 -0400

    REFACTOR: publish identity when is created.

--- CBP/plugin/identity/fermat-cbp-plugin-identity-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/identity/crypto_customer/developer/bitdubai/version_1/CryptoCustomerIdentityPluginRoot.java
+++ CBP/plugin/identity/fermat-cbp-plugin-identity-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/identity/crypto_customer/developer/bitdubai/version_1/CryptoCustomerIdentityPluginRoot.java
@@ -142,7 +142,17 @@ public class CryptoCustomerIdentityPluginRoot extends AbstractPlugin implements 
             cryptoCustomerIdentityDatabaseDao.createNewCryptoCustomerIdentity(cryptoCustomer, keyPair.getPrivateKey(), loggedUser, accuracy, frequency);
 
             broadcaster.publish(BroadcasterType.UPDATE_VIEW, "cambios_en_el_identity_customer_creado");
-
+            //Updated by Manuel
+            //Exposing identity when is created.
+            CryptoCustomerExposingData cryptoCustomerExposingData = new CryptoCustomerExposingData(
+                    cryptoCustomer.getPublicKey(),
+                    cryptoCustomer.getAlias(),
+                    cryptoCustomer.getProfileImage(),
+                    locationManager.getLocation(),
+                    cryptoCustomer.getFrequency().getRefreshInterval(),
+                    0);
+            cryptoCustomerANSManager.exposeIdentity(cryptoCustomerExposingData);
+            //End of update
             return cryptoCustomer;
 
         } catch (CantGetLoggedInDeviceUserException e) {
